5f56785ad12ee5b3fd26fb1f4c249f81853b0c4d
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-08-11 14:02:40 -0300

    feat: create movies schema table

---
+++ src/db/schemas/movies.ts
@@ -0,0 +1,23 @@
+import {
+  pgTable,
+  text,
+  varchar,
+  uuid,
+  date,
+  integer,
+  timestamp,
+} from 'drizzle-orm/pg-core'
+
+export const movies = pgTable('movies', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  title: varchar('title', { length: 255 }).notNull(),
+  originalTitle: varchar('original_title', { length: 255 }),
+  imageUrl: varchar('image_url', { length: 255 }),
+  description: text('description'),
+  releaseDate: date('release_date'),
+  duration: integer('duration'),
+  genre: varchar('genre', { length: 100 }).notNull(),
+  director: varchar('director', { length: 255 }),
+  createdAt: timestamp('created_at').defaultNow(),
+  updatedAt: timestamp('updated_at').defaultNow(),
+})
