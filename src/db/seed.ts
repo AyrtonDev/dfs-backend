3a04fbb6896fbd401cd40f611670a34868626b73
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-08-11 16:54:36 -0300

    feat: create seed config

---
+++ src/db/seed.ts
@@ -0,0 +1,28 @@
+import { reset, seed } from 'drizzle-seed'
+import { schema } from './schemas'
+import { db, pool } from './connection'
+import { images } from '../s3/images'
+
+await reset(db, schema)
+
+await seed(db, schema).refine(f => {
+  return {
+    movies: {
+      count: 20,
+      columns: {
+        title: f.companyName(),
+        originalTitle: f.jobTitle(),
+        description: f.loremIpsum(),
+        genre: f.firstName(),
+        director: f.fullName(),
+        releaseDate: f.date(),
+        duration: f.int({ minValue: 30, maxValue: 180 }),
+        imageUrl: f.default({ defaultValue: images.example }),
+      },
+    },
+  }
+})
+
+await pool.end()
+
+console.log('Database seeded')
