bd17b58c1c1b60c88de708b4a5fd2026d73225eb
Ayrton Guerra <ayrton.guerra@eemovel.com>
2025-08-12 22:30:56 -0300

    fix: remove duplicate

--- src/routes/images/list.ts
+++ src/routes/images/list.ts
@@ -5,35 +5,30 @@ import { bucket } from '../../s3/config'
 import { env } from '../../env'
 
 export const listImagesRoute: FastifyPluginCallbackZod = app => {
-  app.get(
-    '/images',
-    { preHandler: [authMiddleware] },
-    async (request, reply) => {
-      try {
-        const result = await bucket.send(
-          new ListObjectsV2Command({
-            Bucket: `${env.R2_PUBLIC}/post-movie-images`,
-          }),
-        )
+  app.get('/images', { preHandler: [authMiddleware] }, async (_, reply) => {
+    try {
+      const result = await bucket.send(
+        new ListObjectsV2Command({
+          Bucket: env.R2_NAME,
+          Prefix: env.R2_PREFIX,
+        }),
+      )
 
-        if (!result.Contents) {
-          return reply.send([])
-        }
-
-        const images = result.Contents.map(file => ({
-          key: file.Key,
-          lastModified: file.LastModified,
-          size: file.Size,
-          url: `${env.R2_PUBLIC}/post-movie-images/${file.Key}`,
-        }))
-
-        return reply.send(images)
-      } catch (err) {
-        console.log(err)
-        return reply
-          .status(500)
-          .send({ message: 'Something problem happened.' })
+      if (!result.Contents) {
+        return reply.send([])
       }
-    },
-  )
+
+      const images = result.Contents.map(file => ({
+        key: file.Key,
+        lastModified: file.LastModified,
+        size: file.Size,
+        url: `${env.R2_PUBLIC}/${file.Key}`,
+      }))
+
+      return reply.send(images)
+    } catch (err) {
+      console.log(err)
+      return reply.status(500).send({ message: 'Something problem happened.' })
+    }
+  })
 }
